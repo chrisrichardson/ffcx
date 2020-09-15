ad2e5a2b466f806968d5a90f3b4c6a15832c0261
Chris Richardson <chris@bpi.cam.ac.uk>
2020-09-15 10:28:28 +0100

    Use importlib.metadata to get version

--- ffcx/__init__.py
+++ ffcx/__init__.py
@@ -11,13 +11,17 @@ FFC compiles finite element variational forms into C code.
 """
 
 import logging
-
+from importlib.metadata import version, PackageNotFoundError
 import pkg_resources
 
 # Import default parameters
 from ffcx.parameters import default_parameters  # noqa: F401
 
-# __version__ = pkg_resources.get_distribution("fenics-ffcx").version
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # package is not installed
+   pass
 
 logging.basicConfig()
 logger = logging.getLogger("ffcx")
